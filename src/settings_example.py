423e27417f870341d32cc910ffeb9bde782a3193
Donaldduck8 <donaldgit@yandex.com>
2023-04-02 01:38:10 +0200

    Update settings_example.py

--- src/settings_example.py
+++ src/settings_example.py
@@ -1,8 +1,16 @@
+import os
+import json
+import login
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+ODFE_DIR = os.path.join(os.path.dirname(HERE), "odfe")
+
 DISCORD_BOT_TOKEN = ""
 DISCORD_GUILD_ID = ""
 
 DISCORD_CHANNELS_ALLOWED = [
-    "1234567890"
+    ""
 ]
 
 ODFE_VIDEO_CONFIG = {
@@ -30,12 +38,30 @@ ODFE_VIDEO_CONFIG = {
     "cl_aviFrameRate": "60"
 }
 
-STREAMABLE_HEADERS = {}
-
 STREAMABLE_COOKIES = {}
+STREAMABLE_HEADERS = {}
+CONTEXT_COOKIES = []
 
-CONTEXT_COOKIES = [{"name": k, "value": v, "url": "https://streamable.com"} for k,v in STREAMABLE_COOKIES.items()]
+def load_streamable_data():
+    # Get streamable cookies
+    streamable_cookies_p = os.path.join(HERE, "streamable_cookies.json")
 
+    # If cookies don't exist, make user log in
+    while not os.path.isfile(streamable_cookies_p):
+        login.run()
+
+    with open(streamable_cookies_p, "r") as f:
+        global STREAMABLE_COOKIES
+        STREAMABLE_COOKIES = json.loads(f.read())
+
+    # Headers can be empty aside from the Cookie field
+    global STREAMABLE_HEADERS
+    STREAMABLE_HEADERS = {}
+    STREAMABLE_HEADERS["Cookie"] = "; ".join([f"{k}={v}" for k,v in STREAMABLE_COOKIES.items()])
+
+    # Playwright context cookies
+    global CONTEXT_COOKIES 
+    CONTEXT_COOKIES = [{"name": k, "value": v, "url": "https://streamable.com"} for k,v in STREAMABLE_COOKIES.items()]
 
 def convert_config_dic_to_lines(config):
     lines = []
@@ -60,6 +86,8 @@ def convert_config_dic_to_lines(config):
 
 ODFE_VIDEO_MAX_FILE_SIZE_BITS = 250 * 1024 * 1024 * 8
 ODFE_AUDIO_BITRATE = 160000  # ATTENTION: This is defined in the config as well, which you will have to change if you change this
+
+load_streamable_data()
 
 if __name__ == "__main__":
     print(convert_config_dic_to_lines(ODFE_VIDEO_CONFIG))
